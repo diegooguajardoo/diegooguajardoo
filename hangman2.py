7dc610eb1252031cee3691a257889dd88a8d3bf1
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-23 21:45:28 -0600

    ltsleft variable delete

--- hangman2.py
+++ hangman2.py
@@ -4,8 +4,6 @@ listword = []
 listword = ("_ " * len(word)).split()
 pword = ""
 
-ltsleft = len(word)
-
 while score > 0:
 	guess = input("Guess a letter: ")
 	n = 0
@@ -16,13 +14,12 @@ while score > 0:
 		if sample == guess:
 			listword[n] = guess
 			found = True
-			ltsleft -= 1
 		n += 1
 
 	if found == False:
 		score -=1
 
-	if ltsleft == 0:
+	if "_" not in listword:
 		print("You win.")
 		score = 0
 	for i in listword:
